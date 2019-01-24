1e3e6377841bcd2c22f8c70516a57ced02440e58
Scoot <scoot@iPRs-iMac.local>
2019-01-23 19:03:37 -0600

    Initialized Dir as with git. Set up server and esj files. Connected css to esj

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.get("/", function(req, res) {
+  var today = new Date();
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { kindOfDay: day, newListItem: items });
+});
+
+app.post("/", function(req, res) {
+  var item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
+  console.log(item);
+});
+
+app.listen(port, function() {
+  console.log("Server started on port " + port);
+});
